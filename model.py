f7de7d790b3bed83422e99baf88eb318494dbff8
DeokJin <33983084+qute012@users.noreply.github.com>
2020-11-03 17:14:59 +0900

    Create model.py

---
+++ model.py
@@ -0,0 +1,51 @@
+class KWS(nn.Module):
+    def __init__(self, num_classes=len(CLASSES), in_channel=1, hidden_dim=128, n_head=4):
+        super(KWS, self).__init__()
+        self.num_classes = num_classes
+        self.n_head = n_head
+        self.hidden_dim = hidden_dim
+        
+        self.cnn_extractor = nn.Sequential(
+            nn.Conv2d(in_channel, 45, (3,1), stride=2),
+            nn.BatchNorm2d(45),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(45, 45, (3,1), stride=2),
+            nn.BatchNorm2d(45),
+            nn.AvgPool2d(2),
+        )
+        
+        self.rnn = nn.LSTM(45,self.hidden_dim, num_layers=2, bidirectional=True, 
+                             batch_first=True)
+        self.q_emb = nn.Linear(self.hidden_dim<<1, (self.hidden_dim<<1)*self.n_head)
+        self.dropout = nn.Dropout(0.2)
+        
+        self.fc = nn.Sequential(
+            nn.Linear(1024,64),
+            nn.ReLU(True),
+            nn.Linear(64,32),
+            nn.Linear(32,self.num_classes)
+        )
+        #self.softmax = nn.LogSoftmax(dim=-1)
+        
+    def forward(self, x):
+        batch_size = x.size(0)
+        
+        x = self.cnn_extractor(x)
+        x = x.reshape(x.size(0),-1,x.size(1))
+        x,_ = self.rnn(x)
+        
+        middle = x.size(1)//2
+        mid_feature = x[:,middle,:]
+        
+        multiheads = []
+        queries = self.q_emb(mid_feature).view(self.n_head, batch_size, -1, self.hidden_dim<<1)
+        for query in queries:
+            att_weights = torch.bmm(query,x.transpose(1, 2))
+            att_weights = F.softmax(att_weights, dim=-1)
+            multiheads.append(torch.bmm(att_weights, x).view(batch_size,-1))
+        x = torch.cat(multiheads, dim=-1)
+        x = self.dropout(x)
+        
+        x = self.fc(x)
+        
+        return x
